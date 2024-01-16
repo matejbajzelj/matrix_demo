0ca83c3d2e7ab95f45b44a7d6513b30e106ed4e8
Matej Bajzelj <matej.bajzelj@gmail.com>
2024-01-16 13:12:31 +0100

    Add modified code for password
    server and clients are listening and responding to welcome message, password.

--- client.py
+++ client.py
@@ -6,16 +6,46 @@ def start_client(host='127.0.0.1', port=65432):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
 
-        # Prepare message with a custom binary protocol
-        data_to_sent = encode_message(1, f"Hello, Server!")
-        s.sendall(data_to_sent)
-
-        # Receive a response
+        # Receive the welcome message from the server
         data = s.recv(1024)
         message_type, message_length, message = decode_message(data)
+        print(f"Received message from server: {message}")
         print(f"Received message_type: {message_type}")
         print(f"Received message_length: {message_length}")
-        print(f"Received message: {message}")
+
+        # Prompt for the password
+        password = input("Enter your password: ")
+
+        # Prepare and send the password with the custom binary protocol
+        password_data = encode_message(1, password)
+        s.sendall(password_data)
+
+        data = s.recv(1024)
+        message_type, message_length, response = decode_message(data)
+
+        if message_type == 200:
+     
+            while True:
+                # Prompt for user input
+                message = input("Enter a message to send to the server (or 'exit' to quit): ")
+
+                if message == 'exit':
+                    break  # Exit the loop and close the client
+
+                # Prepare and send the message with the custom binary protocol
+                data_to_send = encode_message(1, message)
+                s.sendall(data_to_send)
+
+                # Receive a response
+                data = s.recv(1024)
+                message_type, message_length, response = decode_message(data)
+                print(f"Received message from server: {response}")
+                print(f"Received message_type: {message_type}")
+                print(f"Received message_length: {message_length}")
+        else:
+              # It's an error message (incorrect password)
+            print(f"response from server: {response}")
+            print("Connection closed.")
 
 if __name__ == "__main__":
     start_client()
--- server.py
+++ server.py
@@ -2,9 +2,14 @@ import socket
 import threading
 from tools.tools import decode_message, encode_message
 
+# Define the correct password
+correct_password = "mypass123"
+
 def listen_clients(conn, addr):
     try:
         print(f"Connected by {addr}")
+        # Set message to connected client
+        # Send a response
 
         while True:
             data = conn.recv(1024)
@@ -26,6 +31,7 @@ def listen_clients(conn, addr):
         conn.close()
         print(f"Connection with {addr} closed")
 
+
 def start_server(host='127.0.0.1', port=65432):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
@@ -35,8 +41,29 @@ def start_server(host='127.0.0.1', port=65432):
 
         while True:
             conn, addr = s.accept()
-            client_thread = threading.Thread(target=listen_clients, args=(conn, addr))
-            client_thread.start()
+            
+            welcome_message = f"Initiating the communication. Client {addr} Send password"
+            response = encode_message(0, welcome_message)
+            conn.sendall(response)
+
+             # Receive the password from the client
+            data = conn.recv(1024)
+            message_type, message_length, received_password = decode_message(data)
+            if received_password == correct_password:
+                 # Password is correct, send a success message and keep the connection alive
+                success_message = "Password accepted. Connection established."
+                success_message_data = encode_message(2, success_message)
+                conn.sendall(success_message_data)
+
+                client_thread = threading.Thread(target=listen_clients, args=(conn, addr))
+                client_thread.start()
+
+            else:
+                # Password is incorrect, send an error message and close the connection
+                error_message = "Incorrect password. Connection closed."
+                error_message_data = encode_message(401, error_message)
+                conn.sendall(error_message_data)
+                conn.close()
 
 if __name__ == "__main__":
     start_server()
