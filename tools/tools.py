3127847bbbf6c3dd046084ed7e261edc11b10ca1
Matej Bajzelj <matej.bajzelj@gmail.com>
2024-01-16 13:11:53 +0100

    Change my header to 4 bytes.
    I decided I need message_type of 2 bytes if I want to sent back 401 - example not authenticated.

--- tools/tools.py
+++ tools/tools.py
@@ -1,4 +1,9 @@
 import struct
+my_encode_format = '>HH'
+
+# picked message type and size, picked message length and size of var to make my header.
+# don't know at this time if I need message-type but starting somewhere.
+# types could be "ping or heartbeat", "error", "auth", "game related message"...
 
 def encode_message(message_type, payload):
     # message_type is an integer (0-255), payload is in string
@@ -8,13 +13,13 @@ def encode_message(message_type, payload):
     # Big-endian: 1 byte for type, 2 bytes for length. 
     # B 8bit-int = unsigned char since I need only positive values for message_type, 
     # H 16bit int - Assume for demo 0-65535 is enough big int.
-    header = struct.pack('>BH', message_type, message_length) 
+    header = struct.pack(my_encode_format, message_type, message_length) 
     return header + binary_payload
 
 def decode_message(binary_data):
     
     # Assuming binary_data is a bytes object containing a full message
     # first 3 bytes are (1 message type, 2 payload length)
-    message_type, message_length = struct.unpack('>BH', binary_data[:3])
-    message = binary_data[3:3+message_length]
+    message_type, message_length = struct.unpack(my_encode_format, binary_data[:4])
+    message = binary_data[4:4+message_length]
     return message_type, message_length, message
