9dafd03adc791fb8cebb97ee32eb583fcb2e2f27
Matej Bajzelj <matej.bajzelj@gmail.com>
2024-01-16 12:42:22 +0100

    Add a thread to handle constant listening for clients

--- server.py
+++ server.py
@@ -1,5 +1,30 @@
 import socket
+import threading
 from tools.tools import decode_message, encode_message
+
+def listen_clients(conn, addr):
+    try:
+        print(f"Connected by {addr}")
+
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+
+            # Unpack received data
+            message_type, message_length, message = decode_message(data)
+            print(f"Received message from {addr}: {message}")
+            print(f"Received message_type: {message_type}")
+            print(f"Received message_length: {message_length}")
+
+            # Send a response
+            response = encode_message(1, f"Received your message: {message}")
+            conn.sendall(response)
+    except Exception as e:
+        print(f"Error handling client {addr}: {str(e)}")
+    finally:
+        conn.close()
+        print(f"Connection with {addr} closed")
 
 def start_server(host='127.0.0.1', port=65432):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -8,23 +33,10 @@ def start_server(host='127.0.0.1', port=65432):
 
         print(f"Server listening on {host}:{port}")
 
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-
-                # Unpack received dataI pick
-                message_type, message_length, message = decode_message(data)
-                print(f"Received message: {message}")
-                print(f"Received message_type: {message_type}")
-                print(f"Received message_length: {message_length}")
-
-                # Send a response
-                response = encode_message(1, f"Received your message: {message}")
-                conn.sendall(response)
+        while True:
+            conn, addr = s.accept()
+            client_thread = threading.Thread(target=listen_clients, args=(conn, addr))
+            client_thread.start()
 
 if __name__ == "__main__":
     start_server()
